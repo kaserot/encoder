c9ed66f0c1b92867a58d2d87ce4ebcad4be3e349
kaserot <26345451+kaserot@users.noreply.github.com>
2018-01-01 20:17:01 +0900

    Supported trimming
    - and fix bug

--- encoder.bat
+++ encoder.bat
@@ -6,23 +6,38 @@ set input_file=%1
 set suffix=_output.mp4
 set output_file=%input_file:~0,-4%%suffix%
 
+set/P is_trimming="切り出す?(y/n)"
+if "%is_trimming%"=="y" (
+  set/P start_time="どこから？（秒）"
+  set/P end_time="どこまで？（秒）"
+  set /a trimming_time=end_time - start_time
+  set trim_start_option=-ss !start_time!
+  set trim_end_option=-t !trimming_time!
+)
+
 set/P is_speed_change="速度変更する？(y/n)"
 if "%is_speed_change%"=="y" (
-  set/P speed_option="何倍速？（0.5～2）"
-  set ffmpeg_option=setpts=PTS/!speed_option! -af atempo=!speed_option!
+  set/P reproduction_speed="何倍速？（0.5～2）"
+  set speed_option=setpts=PTS/!reproduction_speed! -af atempo=!reproduction_speed!
 )
 
 set/P is_resize="resize?(y/n)"
 if "%is_resize%"=="y" (
   set/P x="横幅（例：1920/1600/1280/1024/800）"
-  set ffmpeg_option=scale=!x!:-1,%ffmpeg_option%
+  set resize_option=scale=!x!:-1
+)
+
+if "%is_speed_change%"=="y" if "%is_resize%"=="y" (
+  set ffmpeg_option=%speed_option%,%resize_option%
+) else (
+  set ffmpeg_option=%speed_option%%resize_option%
 )
 
 if not "%ffmpeg_option%."=="." (
   set ffmpeg_option=-vf %ffmpeg_option%
 )
 
-ffmpeg.exe -i %input_file% -vcodec h264_nvenc %ffmpeg_option% %output_file%
+ffmpeg.exe %trim_start_option% -i %input_file% %trim_end_option% -vcodec h264_nvenc %ffmpeg_option% %output_file%
 rem ffmpeg.exe -i "hogehoge.mp4" -vcodec h264_nvenc -vf setpts=PTS/2.0 -af atempo=2.0 dest.mp4
 
 pause
